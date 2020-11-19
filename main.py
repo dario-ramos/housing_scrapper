793383588928001e96a8d4e9485c0b949421cfd3
Dario Ramos <dario.fenix@gmail.com>
2020-11-18 23:06:32 -0300

    Print traceback

--- main.py
+++ main.py
@@ -3,13 +3,15 @@
 import logging
 import yaml
 import sys
+import traceback
 from notifier import Notifier
 from providers.processor import process_properties
 
 # logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-# configuration    
+# configuration
 with open("configuration.yml", 'r') as ymlfile:
     cfg = yaml.safe_load(ymlfile)
 
@@ -20,10 +22,13 @@ for provider_name, provider_data in cfg['providers'].items():
     try:
         logging.info(f"Processing provider {provider_name}")
         new_properties += process_properties(provider_name, provider_data)
-    except:
-        logging.error(f"Error processing provider {provider_name}.\n{sys.exc_info()[0]}")
-
-if len(new_properties) > 0:
-    notifier.notify(new_properties)
-else:
-    logging.info("No new properties found :(")
+        if len(new_properties) > 0:
+            notifier.notify(new_properties)
+            logging.info(
+                f"Done! Found {len(new_properties)} new properties :)")
+        else:
+            logging.info("No new properties found :(")
+    except Exception as e:
+        logging.error(
+            f"Error processing provider {provider_name}.\n{sys.exc_info()[0]}")
+        print(''.join(traceback.format_exception(None, e, e.__traceback__)))
