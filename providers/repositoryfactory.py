02703eee40dc6f577fd7252392603fc4c9d16b98
Dario Ramos <dario.fenix@gmail.com>
2020-12-21 04:03:19 -0300

    Fix factory

--- providers/repositoryfactory.py
+++ providers/repositoryfactory.py
@@ -6,15 +6,15 @@ def get_factory(cfg):
     def create_sql_lite_repository():
         return SqliteRepository('properties.db')
 
-    def create_herokupg_repository(conn_info):
-        return HerokuPgRepository(conn_info)
+    def create_herokupg_repository():
+        conn_uri = getDbUriFromHeroku(cfg.heroku_app_name())
+        return HerokuPgRepository(conn_uri)
 
     store = cfg.database_store()
     if store == 'localsqlite':
         return create_sql_lite_repository
     elif store == 'herokupg':
-        conn_uri = getDbUriFromHeroku(cfg.heroku_app_name())
-        return create_herokupg_repository(conn_uri)
+        return create_herokupg_repository
 
     else:
         raise LookupError(
